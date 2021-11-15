5b24e9b3b775bb3a5307e1064d73273cc52bcef8
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 13:55:58 -0300

    add BaconDecorator

---
+++ DecoratorPattern/BaconDecorator.cs
@@ -0,0 +1,19 @@
+﻿using DecoratorPattern.Interfaces;
+
+namespace DecoratorPattern
+{
+    public class BaconDecorator : PizzaDecorator
+    {
+        public BaconDecorator(IPizza pizza) : base(pizza)
+        {
+
+        }
+        public override string BuscarTipo()
+        {
+
+            var pizza =  base.BuscarTipo();
+            pizza += "\r\n com extra de bacon";
+            return pizza;
+        }
+    }
+}
