6330f68da4dca91fdb7846877e8f7508e14c0991
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 13:44:00 -0300

    add Pizza decorator

---
+++ DecoratorPattern/PizzaDecorator.cs
@@ -0,0 +1,19 @@
+﻿using DecoratorPattern.Interfaces;
+namespace DecoratorPattern
+{
+    public class PizzaDecorator : IPizza
+    {
+        private readonly IPizza _pizza;
+
+        public PizzaDecorator(IPizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public virtual string BuscarTipo()
+        {
+            var pizza = _pizza.BuscarTipo();
+            return pizza;
+        }
+    }
+}
