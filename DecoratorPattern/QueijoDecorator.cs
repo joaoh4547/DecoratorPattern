2e01c742e118f4087aed3ba725dab02758a45083
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 13:49:31 -0300

    add QueijoDEcorator

---
+++ DecoratorPattern/QueijoDecorator.cs
@@ -0,0 +1,18 @@
+﻿using DecoratorPattern.Interfaces;
+
+namespace DecoratorPattern
+{
+    public class QueijoDecorator : PizzaDecorator
+    {
+        public QueijoDecorator(IPizza pizza) : base(pizza)
+        {
+        }
+
+        public override string BuscarTipo()
+        {
+            var pizza = base.BuscarTipo();
+            pizza += "\r\n com extra de queijo";
+            return pizza;
+        }
+    }
+}
