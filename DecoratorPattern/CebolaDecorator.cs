cdd8dc159bb57cb427695f36b94c6c285665a016
João Henrique <joaohenriquetdj@gmail.com>
2021-11-15 13:59:10 -0300

    add CebolaDecorator

---
+++ DecoratorPattern/CebolaDecorator.cs
@@ -0,0 +1,19 @@
+﻿using DecoratorPattern.Interfaces;
+
+namespace DecoratorPattern
+{
+    public class CebolaDecorator : PizzaDecorator
+    {
+        public CebolaDecorator(IPizza pizza) : base(pizza)
+        {
+
+        }
+
+        public override string BuscarTipo()
+        {
+            var pizza = base.BuscarTipo();
+            pizza += "\r\n com extra de cebola";
+            return pizza;   
+        }
+    }
+}
